a93165c480141842cf62aa3f126533a76d95a3ef
acmcgowan2”git config --global user.email “acmcgowan2@gmail.com” <student@Cumberlandss-MacBook-Pro-5.local>
2022-03-01 23:53:43 -0600

    added Variables Exercise

---
+++ script.js
@@ -0,0 +1,34 @@
+// const
+
+let num = 20; 
+// console.log(num); // 20
+
+num = 15; 
+// console.log(num); // 15
+
+num = num + 7; 
+// console.log(num); // 22
+
+num --;
+// console.log(num); // 21
+
+num = "This is not a number";
+// console.log(num); // This is not a number 
+
+let blank;
+// console.log(blank); // Undefined
+
+blank = null;
+// console.log(blank); // null
+
+// BONUS!!!
+
+const quarter = 0.25 , dime = 0.10 , nickel = 0.05 , penny = 0.01;
+
+// console.log(quarter, dime, nickel, penny); // 0.25 0.1 0.05 0.01
+
+let letters = "qwertyuiop";
+// console.log(letters); // qwertyuiop
+
+console.log(letters.length); //10
+console.log(letters[9]); // p
